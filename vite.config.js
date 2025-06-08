13ac542ac656896e954ed5f47a7568210333a07c
Ajay Singh <90156893+Ajay080@users.noreply.github.com>
2025-06-08 10:54:39 +0530

    fixing build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {                        // ← add alias here
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   esbuild: {
